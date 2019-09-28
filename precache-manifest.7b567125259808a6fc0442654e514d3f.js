45146093eb4a78043387de3078f7161fd2d2f30a
Natalija Jovanovska <natalija.j@live.com>
2019-09-28 15:00:33 +0200

    Updates

---
+++ precache-manifest.7b567125259808a6fc0442654e514d3f.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "16a03c09522100bc5a0b5e4f0afcd95e",
+    "url": "/lamps/index.html"
+  },
+  {
+    "revision": "efd001062de7e35d2b3a",
+    "url": "/lamps/static/css/main.c29f2fe6.chunk.css"
+  },
+  {
+    "revision": "23ccc5bfc4bd7fc92cc2",
+    "url": "/lamps/static/js/2.dc4e6dbb.chunk.js"
+  },
+  {
+    "revision": "efd001062de7e35d2b3a",
+    "url": "/lamps/static/js/main.d028538a.chunk.js"
+  },
+  {
+    "revision": "1a92380ae1940e227a79",
+    "url": "/lamps/static/js/runtime-main.981dc26c.js"
+  },
+  {
+    "revision": "3e5825ead2238a34789e5082208e2d31",
+    "url": "/lamps/static/media/1.3e5825ea.jpg"
+  },
+  {
+    "revision": "2cedaac85b5d2ead3c95df9996699813",
+    "url": "/lamps/static/media/3.2cedaac8.jpg"
+  },
+  {
+    "revision": "db8a3bcf1c00794b78f69c53c2ee3e56",
+    "url": "/lamps/static/media/4.db8a3bcf.jpg"
+  },
+  {
+    "revision": "06086aa7a29ba423c786cbcf20ddda6e",
+    "url": "/lamps/static/media/6.06086aa7.jpg"
+  },
+  {
+    "revision": "d5eef952b96565e77cb87d4b683fc942",
+    "url": "/lamps/static/media/7.d5eef952.jpg"
+  },
+  {
+    "revision": "4f2b8daa0e3f0565f17dd06ed9e8497d",
+    "url": "/lamps/static/media/8.4f2b8daa.jpg"
+  }
+]);
